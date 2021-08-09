4c75ffaae6c28d032b22baa16d07e0b535b9bbe8
gamaray123 <83606175+gamaray123@users.noreply.github.com>
2021-08-09 19:54:29 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+
+
+
+
+<?php
+$username = $_GET["username"];
+$plain_password = $_GET["password"];
+$secure_password = md5($plain_password);
+$conn = new mysqli("localhost", "hamakins", "password123", "Users");
+if ($conn->connect_error) {
+    die("Failed to connect to database: ". $conn->connect_error);
+}
+$check = "SELECT User, Pass FROM Users WHERE User = '$username';";
+$result = $conn->query($check);
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    if ($row["Pass"] == $secure_password) {
+      echo "<h1 style='background: green;'>Login success</h1><br><br>";
+      echo "<h3>Welcome back $username<h3>";
+    } else {
+      echo "Incorrect Username or Password";
+    }
+  }
+} else {
+  echo "Incorrect Username or Password";
+}
+$conn->close();
+?>
+
