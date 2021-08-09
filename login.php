1c8ee03597ad1333dd3b0228c8c7e645e8e5c529
gamaray123 <83606175+gamaray123@users.noreply.github.com>
2021-08-09 19:55:54 +0000

    Update login.php

--- login.php
+++ login.php
@@ -6,11 +6,11 @@
 $username = $_GET["username"];
 $plain_password = $_GET["password"];
 $secure_password = md5($plain_password);
-$conn = new mysqli("localhost", "hamakins", "password123", "Users");
+$conn = new mysqli("localhost", "username", "password", "table");
 if ($conn->connect_error) {
     die("Failed to connect to database: ". $conn->connect_error);
 }
-$check = "SELECT User, Pass FROM Users WHERE User = '$username';";
+$check = "SELECT column1, column2 FROM table WHERE column1 = '$username';";
 $result = $conn->query($check);
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
