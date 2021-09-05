08ac559e11d9771669a8b73ddb5800f50cc16e4e
gamaray123 <83606175+gamaray123@users.noreply.github.com>
2021-09-05 17:22:35 +0000

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -1,17 +1,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
-$message = "Password Reset: 127.0.0.1/login/reset_pass.html";
 $email = $_GET['email'];
-$conn = new mysqli("localhost", "hamakins", "password123", "Users");
+$conn = new mysqli("localhost", "user", "password", "Users");
 if ($conn->connect_error) {
   die("Failed to connect to database: ". connect_error);
 }
 $sql = "SELECT email FROM Site_Users WHERE email = '$email';";
 $result = $conn->query($sql);
 if ($result-> num_rows > 0) {
-  echo "<h1>If that email is on our servers we will send a lin to it<h1>";
-  mail($email, "Password Reset", $message);
+  echo "<h1>Sorry, password resets are not currently working<h1>";
 } else {
-  echo "<h1>If that email is on our servers we will send a link to it<h1>";
+  echo "<h1>Sorry, password resets are not currently working<h1>";
 }
 ?>
