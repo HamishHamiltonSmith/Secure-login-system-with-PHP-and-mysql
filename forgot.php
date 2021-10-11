fc6fc7742fa7b82dbac6f77fa0af7b14faf2d0a1
Hamish Hamilton-Smith <83606175+HamishHamiltonSmith@users.noreply.github.com>
2021-10-11 18:36:03 +0000

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -1,7 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 $email = $_GET['email'];
-$conn = new mysqli("localhost", "user", "password", "Users");
+$conn = new mysqli("localhost", "username", "password", "database");
 if ($conn->connect_error) {
   die("Failed to connect to database: ". connect_error);
 }
