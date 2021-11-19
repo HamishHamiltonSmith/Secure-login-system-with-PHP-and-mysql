697979b8450c016dfff53d5221aeb33b9951541c
Hamish Hamilton-Smith <83606175+HamishHamiltonSmith@users.noreply.github.com>
2021-11-19 15:25:39 +0000

    Added prepared statements

--- add_user.php
+++ add_user.php
@@ -1,19 +1,27 @@
 
 <?php
+$conn = new mysqli("localhost", "username", "password", "db");
+if ($conn->connect_error) {
+  die("Falied to conect to database: ". $conn->connect_error);
+}
+
 $username = $_GET["username"];
 $plain_pass = $_GET["password"];
 $hash_pass = md5($plain_pass);
 $email = $_GET["email"];
-$conn = new mysqli("localhost", "username", "password", "database");
-if ($conn->connect_error) {
-  die("Falied to conect to database: ". $conn->connect_error);
-}
-$add = "INSERT INTO Site_Users (username, email, password) VALUES ('$username', '$email', '$hash_pass');";
-if ($conn->query($add) === TRUE) {
+
+$add = $conn->prepare("INSERT INTO table (username, email, password) VALUES (?, ?, ?)");
+$add->bind_param("sss", $username, $email, $hash_pass);
+
+
+
+
+if ($add->execute() === TRUE) {
   echo "<h1>Account Created!</h1><br>";
   echo "<a href='login-page.html'>Back to login</a>";
 } else {
   echo "Could not create user: ". $conn->error;
 }
 $conn->close();
+$add->close();
 ?>
